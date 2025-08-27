3b25c52f6c3c905631e5dedf03ddaf34a5b385b5
Dilker15 <dilker72@gmail.com>
2025-08-26 23:32:39 -0300

    swagger added

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -20,7 +21,8 @@ import { TypeOrmModule } from '@nestjs/typeorm';
          synchronize:true,
          autoLoadEntities:true,
       })
-    })
+    }),
+    AuthModule
 
   ],
   controllers: [AppController],
