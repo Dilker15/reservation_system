ee811daef34a093efd2c285ec4b1b94ae40f8e24
Dilker15 <dilker72@gmail.com>
2025-08-26 22:33:48 -0300

    configuration db postgres

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.APP_PORT ?? 4000);
+
+
+
+  console.log(`APP IS RUNNING ON PORT :  ${process.env.APP_PORT}`);
 }
 bootstrap();
