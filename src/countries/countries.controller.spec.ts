8f8ff2eeefffd6eacd475be99ef1b21d7e1597b9
Dilker15 <dilker72@gmail.com>
2025-10-03 09:47:17 -0300

    test:test workflow unit-test and e2e test

--- src/countries/countries.controller.spec.ts
+++ src/countries/countries.controller.spec.ts
@@ -7,13 +7,16 @@ describe('CountriesController', () => {
   let mockCountryService: Partial<jest.Mocked<CountriesService>>;
 
   const mockCountries = [{name:"Bolivia"},{name:"Argentina"},{name:"España"}];
-
+  const mockCities = ['Santa Cruz',"La Paz"];
 
   beforeEach(async () => {
 
     mockCountryService = {
       insertCountriesAndCities:jest.fn(),
       findAll:jest.fn(),
+      findOneCountry:jest.fn().mockResolvedValue(mockCountries[0]),
+      findCitiesFromCountry:jest.fn().mockResolvedValue(mockCities),
+      findCity:jest.fn().mockResolvedValue(mockCities[0])
     };
 
     const module: TestingModule = await Test.createTestingModule({
@@ -44,12 +47,39 @@ describe('CountriesController', () => {
   it("should call service.findAll and return countries list",async()=>{
 
       mockCountryService.findAll?.mockResolvedValueOnce(mockCountries as any);
-      //const data = await controller.findAll();
+      const data = await controller.findAllCountries();
       expect(mockCountryService.findAll).toHaveBeenCalledTimes(1);
-      //expect(data).toEqual(mockCountries);
+      expect(data).toEqual(mockCountries);
   });
 
 
+
+  it("should call findCountry with ID and return a Country",async()=>{
+     const countryId = 'uuid-test';
+     const data = await controller.findCountry(countryId);
+     expect(mockCountryService.findOneCountry).toHaveBeenCalledWith(countryId);
+     expect(data).toEqual({name:'Bolivia'})
+  });
+
+
+
+  it("should get a list of cities from a country",async()=>{
+      const countryId = 'uuid-test';
+      const cities = await controller.getCitiesFromCountry(countryId);
+      expect(mockCountryService.findCitiesFromCountry).toHaveBeenCalledWith(countryId);
+      expect(cities).toEqual(mockCities);
+  });
+
+
+
+  it("should return data from a active city",async()=>{
+    const countryId = 'uuid-country';
+    const cityId = 'uuid-city';
+    const city = await controller.getCity(countryId,cityId);
+    expect(mockCountryService.findCity).toHaveBeenCalledWith(countryId,cityId);
+    expect(city).toEqual(mockCities[0]);
+
+  });
 
 
 });
