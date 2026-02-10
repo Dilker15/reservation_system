6110febb9890dc5d01634ecf41f728bc701d612e
Dilker15 <dilker72@gmail.com>
2026-02-10 20:38:00 -0300

    feat(reservations):add filter reservations

--- src/reservation/reservation.controller.ts
+++ src/reservation/reservation.controller.ts
@@ -19,12 +19,8 @@ export class ReservationController {
 
   @Role(Roles.CLIENT)
   @Post()
-  create(
-    @Body() createReservationDto: CreateReservationDto,
-    @GetClient() client: User
-  ) {
+  create(@Body() createReservationDto: CreateReservationDto,@GetClient() client: User) {
     return this.reservationService.create(createReservationDto, client);
-  
   }
 
 
@@ -34,10 +30,18 @@ export class ReservationController {
     return this.reservationService.getReservationsList(user,pagination)
   }
 
+
   @Role(Roles.CLIENT)
+  @Get(':id/payment')
+  getReservationPaymentInfo(@Param('id', ParseUUIDPipe) id: string,@GetUser() currentUser:User) {
+     return this.reservationService.getPaymentInfo(id,currentUser);
+  }
+
+
+  @Role(Roles.OWNER,Roles.CLIENT)
   @Get(':id')
-  getOwnerReservation(@Param('id', ParseUUIDPipe) id: string) {
-     return this.reservationService.findOne(id);
+  getReservation(@Param('id',ParseUUIDPipe) reseravtion_id:string,@GetUser() currenUser:User){
+    return this.reservationService.getReservationById(reseravtion_id,currenUser);
   }
 
 
